9a0b52dedbee8b7aa14f1d790175039b40c57e27
ricsi0901 <ricsi.szucs22@gmail.com>
2021-05-15 20:09:27 +0200

    4.feladat

--- public_html/javascript.js
+++ public_html/javascript.js
@@ -1,6 +1,23 @@
+var termekek='[{"id": "1", "nev": "fülhallgató","ár": "5000","db": "10"},{"id": "2","nev": "pendrive 128 gb","ár": "10000","db": "20"}]';
+var adatObjektum=JSON.parse(termekek);
 $(function(){
-    var adatObjektum=JSON.parse(termekek);
+    kiir();
+    
 });
 var termekek='[{"id": "1", "nev": "fülhallgató","ár": "5000","db": "10"},{"id": "2","nev": "pendrive 128 gb","ár": "10000","db": "20"}]';
+function kiir(){
+    var i =1;
+    for (var index in adatObjektum) {
+        $("table ").append("<tr>");
+        console.log(adatObjektum[index]);
+        for (var item in adatObjektum[index]) {
+            $("table tr").eq(i).append("<td>"+adatObjektum[index][item]+"</td>");
+           
+        }
+        $("table tr").eq(i).append('<td><input type="button" id="torol" name="torol" value="töröl"></td>');
+            $("table tr").eq(i).append('<td><input type="button" id="modosit" name="modosit" value="modosit"></td>');
+        i++;
+    }
+}
 
 
